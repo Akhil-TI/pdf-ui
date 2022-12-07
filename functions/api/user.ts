997ce9d6188aba1214621dc9dc64785f8c106948
akhil <akhilenduchaturvedi16@gmail.com>
2022-12-08 00:27:59 +0530

    test

--- functions/api/user.ts
+++ functions/api/user.ts
@@ -2,6 +2,6 @@ interface Env {
     userdata: KVNamespace;
 }
 export const onRequest: PagesFunction<Env> = async (context) => {
-    const task = await context.env.userdata.get("user");
+    const task = await context.env.pdf.get("user");
     return new Response(task);
 }
