756d8157c5ccce2ef9a1d350eadf55132ff9e44b
Akhil <akhilendu.chaturvedi@informa.com>
2023-01-05 16:17:59 +0530

    files

--- src/app/components/ng2-pdf-js/ng2-pdf-js.component.ts
+++ src/app/components/ng2-pdf-js/ng2-pdf-js.component.ts
@@ -5,19 +5,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   templateUrl: './ng2-pdf-js.component.html',
   styleUrls: ['./ng2-pdf-js.component.scss'],
 })
-export class Ng2PdfJsComponent implements OnInit {
-  // path = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
-  // path = 'assets/pdf-test.pdf';
-  path = 'pdf-test.pdf';
-  @ViewChild('externalPdfViewer') public externalPdfViewer:any;
-  constructor() {}
-  public openPdf() {
-    console.log('opening pdf in new tab!');
-    this.externalPdfViewer.pdfSrc = 'pdf-test.pdf';
-    // this.externalPdfViewer.refresh();
-  }
-
-  ngOnInit(): void {
-    // this.openPdf()
-  }
+export class Ng2PdfJsComponent {
+  path = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
 }
