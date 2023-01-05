2535b697cc333f0403f626f9a8e06aa2a76de51e
Akhil <akhilendu.chaturvedi@informa.com>
2023-01-05 16:24:19 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Ng2PdfJsComponent } from './components/ng2-pdf-js/ng2-pdf-js.component';
-import { Ng2Component } from './components/ng2/ng2.component';
-import { NgxComponent } from './components/ngx/ngx.component';
+// import { Ng2Component } from './components/ng2/ng2.component';
+// import { NgxComponent } from './components/ngx/ngx.component';
 
 const routes: Routes = [
-  { path: 'ng2-ui', component: Ng2Component },
-  { path: 'ngx-ui', component: NgxComponent },
+  // { path: 'ng2-ui', component: Ng2Component },
+  // { path: 'ngx-ui', component: NgxComponent },
   { path: 'pdfJs', component: Ng2PdfJsComponent },
   { path: '**', component: Ng2PdfJsComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { InlineSVGModule } from 'ng-inline-svg';
 
 import { AppComponent } from './app.component';
-import { Ng2Component } from './components/ng2/ng2.component';
-import { NgxComponent } from './components/ngx/ngx.component';
+// import { Ng2Component } from './components/ng2/ng2.component';
+// import { NgxComponent } from './components/ngx/ngx.component';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { PdfJsViewerModule } from 'ng2-pdfjs-viewer';
 import { Ng2PdfJsComponent } from './components/ng2-pdf-js/ng2-pdf-js.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, Ng2Component, NgxComponent, Ng2PdfJsComponent],
+  declarations: [AppComponent,
+    //  Ng2Component, NgxComponent, 
+     Ng2PdfJsComponent],
   imports: [
     BrowserModule,
     FormsModule,
